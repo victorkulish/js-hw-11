f53dbc4a0bf3c99e77efb2c19a96fe9d2f5ea293
victorkulish <163741425+victorkulish@users.noreply.github.com>
2024-10-06 16:02:43 +0300

    finish

---
+++ src/js/script.js
@@ -0,0 +1,115 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const formSearch = document.querySelector('.js-search');
+const listImages = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+
+loader.style.display = 'none';
+formSearch.addEventListener('submit', onSearch);
+
+function onSearch(event) {
+  event.preventDefault();
+  listImages.innerHTML = '';
+  loader.style.display = 'block';
+
+  const inputValue = event.target.elements.search.value;
+
+  getPictures(inputValue)
+    .then(data => {
+      loader.style.display = 'none';
+
+      if (!data.hits.length) {
+        iziToast.error({
+          title: 'Error',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+      }
+
+      listImages.innerHTML = ('beforeend', createMarkup(data.hits));
+
+      const refreshPage = new SimpleLightbox('.gallery a', {
+        captions: true,
+        captionsData: 'alt',
+        captionDelay: 250,
+      });
+      refreshPage.refresh();
+
+      formSearch.reset();
+    })
+    .catch(err => {
+      loader.style.display = 'none';
+      console.log(err);
+    });
+}
+
+function getPictures(name) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const KEY = '46359588-09116391d0d94e56de7df57e6';
+
+  if (name.includes(' ')) {
+    name.replace(/\s+/g, '+');
+  }
+
+  const searchParams = new URLSearchParams({
+    key: KEY,
+    q: name,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+
+  return fetch(`${BASE_URL}?${searchParams}`).then(res => {
+    if (!res.ok) {
+      throw new Error(res.statusText);
+    }
+    return res.json();
+  });
+}
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `<li class="gallery-item">
+          <a class="gallery-link" href="${largeImageURL}">
+            <img
+              class="gallery-image"
+              src="${webformatURL}"
+              alt="${tags}"
+              width="360"
+            />
+          </a>
+          <div class="thumb-block">
+            <div class="block">
+              <h2 class="tittle">Likes</h2>
+              <p class="amount">${likes}</p>
+            </div>
+            <div class="block">
+              <h2 class="tittle">Views</h2>
+              <p class="amount">${views}</p>
+            </div>
+            <div class="block">
+              <h2 class="tittle">Comments</h2>
+              <p class="amount">${comments}</p>
+            </div>
+            <div class="block">
+              <h2 class="tittle">Downloads</h2>
+              <p class="amount">${downloads}</p>
+            </div>
+          </div>
+        </li>`
+    )
+    .join('');
+}
