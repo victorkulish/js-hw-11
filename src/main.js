f44d92d9a2c104e0cf5ded2a508dc827b65cbdbf
victorkulish <163741425+victorkulish@users.noreply.github.com>
2024-10-06 17:19:22 +0300

    finish

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { createMarkup } from './js/render-function';
+import { getPictures } from './js/pixabay-api';
 
 const formSearch = document.querySelector('.js-search');
 const listImages = document.querySelector('.gallery');
